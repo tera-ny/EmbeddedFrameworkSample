bc1b517060d64f9fd39e64008f8412b31b04e858
g4zeru <haru25haru02@gmail.com>
2019-04-01 19:52:23 +0900

    add: UsersTest

--- DomainTests/DomainTests.swift
+++ DomainTests/DomainTests.swift
@@ -45,6 +45,41 @@ class DomainTests: XCTestCase {
         
         self.waitForExpectations(timeout: 3, handler: nil)
     }
+    func testUsers() {
+        let page = 1
+        let per = 20
+        let model: APIModel = APIModel(path: "https://qiita.com/api/v2/users?page=\(page)&per_page=\(per)", requestMethod: .get, header: ["Host":"qiita.com"])
+        let networkExpectation: XCTestExpectation? =
+            self.expectation(description: "connect API")
+        let context = NetworkCreator.createContext()
+        debugPrint("request...")
+        context.request(model: model) { (response) in
+            defer {
+                networkExpectation?.fulfill()
+            }
+            debugPrint("connected...")
+            switch response {
+            case .failure(let error):
+                debugPrint(error)
+                XCTAssert(false)
+            case .success(let json):
+                guard let json = json else {
+                    XCTAssert(true)
+                    return
+                }
+                let result = NetworkCreator.decodeToBaseDataModels(json: json, type: User.self)
+                switch result {
+                case .failure(let error):
+                    debugPrint(error)
+                    XCTAssert(false)
+                case .success(let users):
+                    XCTAssertEqual(per, users.count)
+                }
+            }
+        }
+        
+        self.waitForExpectations(timeout: 3, handler: nil)
+    }
 
 }
 func debugPrint(_ item: Any?) {
