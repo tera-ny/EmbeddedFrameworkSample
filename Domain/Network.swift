e4793466252f83f7325fe1d6395c0f1909918b27
g4zeru <33125821+g4zeru@users.noreply.github.com>
2019-04-01 15:40:48 +0900

    Revert "fix: decoder"

--- Domain/Network.swift
+++ Domain/Network.swift
@@ -15,10 +15,9 @@ public protocol Network {
 class NetworkImpl: Network {
     private let cachePolicy: NSURLRequest.CachePolicy = NSURLRequest.CachePolicy.returnCacheDataElseLoad
     private let timeoutInterval: TimeInterval = 20
-    private var requestCount: Int = 0
     
     func request(model: APIModel, completion: @escaping (Result<Data?, Error>) -> Void) {
-        let session = URLSession.shared
+        
         guard let url = URL(string: model.path) else {
             completion(.failure(NSError(domain: "Url isn't active", code: 1, userInfo: nil)))
             return
@@ -33,13 +32,8 @@ class NetworkImpl: Network {
         } catch {
             completion(.failure(error))
         }
-        let task: URLSessionDataTask = session.dataTask(with: request) {(data, response, error) in
-            if let response = response as? HTTPURLResponse {
-                print("StatusCode\(response.statusCode)")
-            } else {
-                print("response:\(String(describing: response))")
-                print(model)
-            }
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let error = error {
                 completion(.failure(error))
             }
@@ -63,24 +57,17 @@ class NetworkImpl: Network {
 class NetworkDummyData: Network {
     func request(model: APIModel, completion: @escaping (Result<Data?, Error>) -> Void) {
         let dummyUserJsonString: String = """
+[
 {
-  "description": "Hello, world.",
-  "facebook_id": "yaotti",
-  "followees_count": 100,
-  "followers_count": 200,
-  "github_login_name": "yaotti",
-  "id": "yaotti",
-  "items_count": 300,
-  "linkedin_id": "yaotti",
-  "location": "Tokyo, Japan",
-  "name": "Hiroshige Umino",
-  "organization": "Increments Inc",
-  "permanent_id": 1,
-  "profile_image_url": "https://si0.twimg.com/profile_images/2309761038/1ijg13pfs0dg84sk2y0h_normal.jpeg",
-  "team_only": false,
-  "twitter_screen_name": "yaotti",
-  "website_url": "http://yaotti.hatenablog.com"
+"userId": "lkddajflksdjflajsdlf",
+"name": "hoge",
+"age": 20
+},{
+"userId": "lkddajfldjksldkjslk",
+"name": "piyo",
+"age": 18
 }
+]
 """
         completion(.success(dummyUserJsonString.data(using: String.Encoding.utf8)))
         //completion(.failure(NSError(domain: "DummyData", code: 1, userInfo: nil)))
