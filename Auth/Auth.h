a04eedfd6909b98d6cb3e608221d2e192813b2f7
g4zeru <haru25haru02@gmail.com>
2019-04-05 12:15:35 +0900

    add: auth

---
+++ Auth/Auth.h
@@ -0,0 +1,19 @@
+//
+//  Auth.h
+//  Auth
+//
+//  Created by iniad on 2019/04/05.
+//  Copyright © 2019 teranyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Auth.
+FOUNDATION_EXPORT double AuthVersionNumber;
+
+//! Project version string for Auth.
+FOUNDATION_EXPORT const unsigned char AuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Auth/PublicHeader.h>
+
+
